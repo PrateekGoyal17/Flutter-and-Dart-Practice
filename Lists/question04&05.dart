e8596bea3c031717fe82e608bb99b8a205ff7adb
Prateek Goyal <96909140+PrateekGoyal17@users.noreply.github.com>
2023-02-28 10:03:44 +0530

    Update question04&05.dart

--- Lists/question04&05.dart
+++ Lists/question04&05.dart
@@ -1,4 +1,7 @@
 void main(){
           List<int> l=[1,2,4,210,200,200];
-          l.forEach((element) => element > 10 ? print("True"): print("False"));
+          bool t1 = l.any((element) => element>10);         //it wil return trur if any element is >10
+          bool t2 = l.every((element) => element>10);       //it will return true if all the elements are >10
+          print(t1);
+          print(t2);
         }
